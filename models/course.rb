3f11b84e6c5e63c82367ffdd2af33f046d2e5585
Veronica Bustabad <vbustabad@gmail.com>
2018-07-11 15:28:04 +0000

    Done.

---
+++ models/course.rb
@@ -0,0 +1,17 @@
+class Course
+
+  attr_reader :name, :topic
+
+  COURSES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @topic = args[:topic]
+    COURSES << self
+  end
+
+  def self.all
+    COURSES
+  end
+
+end
---
+++ models/student.rb
@@ -0,0 +1,17 @@
+class Student
+
+  attr_reader :name, :grade
+
+  STUDENTS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @grade = params[:grade]
+    STUDENTS << self
+  end
+
+  def self.all
+    STUDENTS
+  end
+
+end
